abf4c553fac29f70a111ed38aea0d0aa43b27786
Xabier Larrakoetxea <slok69@gmail.com>
2017-04-28 12:02:20 +0200

    Also make release when tagged
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- travis-build.sh
+++ travis-build.sh
@@ -6,7 +6,7 @@ make vet
 # Run tests
 make test
 
-# Push to docker hub only when master branch build
-if [ $TRAVIS_BRANCH == 'master' ]; then
+# Push to docker hub only when master branch build or release
+if [ $TRAVIS_BRANCH == 'master' ] || ! [ -z ${TRAVIS_TAG} ]; then
     make push
 fi
