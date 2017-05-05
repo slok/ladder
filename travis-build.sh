bc769a38247e1cbcdd5677f8219cc76f4ca5b337
Xabier Larrakoetxea <slok69@gmail.com>
2017-05-05 18:47:46 +0200

    Add trigger of github release on travis
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- travis-build.sh
+++ travis-build.sh
@@ -6,7 +6,8 @@ make vet
 # Run tests
 make test
 
-# Push to docker hub only when master branch build or release
+# Release to docker and github only when master branch build or release
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ ${TRAVIS_BRANCH} == 'master' ] || ! [ -z ${TRAVIS_TAG} ]; then
     make push
+    make gh-release
 fi
